0602ec5e633adac29375a99139f45c28a1e5ec30
qronix <qroniix@gmail.com>
2018-11-02 21:55:30 -0400

    Add form verification for useredit form.

--- public/js/users.js
+++ public/js/users.js
@@ -26,12 +26,58 @@ function cancelEdit(event,ele){
 }
 
 async function submitUserEdit(event,ele){
-    let dashboard = document.getElementById('dashboard');
-    let userid = document.getElementById('userid');
-    let response = await axios.patch(`/users/${userid}`);
+    try{
+        if(checkpasswords()){
+            let data = getFormData();
+            let dashboard = document.getElementById('dashboard');
+            let userid = document.getElementById('userid');
 
-    
+            let response = await axios.patch(`/users/${userid}`,{
+            });
+        }else{
+            throw new Error('Password confirmation failed');
+        }
+    }catch(err){
+        Notification.displayNotification('error',err);
+    }
+}
 
+function getFormData(){
+    const targets = document.querySelectorAll('#usereditPanel input');
+    let data = {};
+    try{
+        targets.forEach((input)=>{
+            let id = input.getAttribute('id');
+            let value = input.value;
+            if(id !=='password' && id!=='confirmPassword'){
+                if(value!==''){
+                    data[id] = value;
+                }else{
+                    let idCaps = id[0].toUpperCase() + id.split(id[0])[1];
+                    throw new Error(`${idCaps} cannot be blank`);
+                }
+            }
+        });
+        data.role = document.getElementById('role').value;
+        console.log(data);
+    }catch(err){
+        Notification.displayNotification('error',err.message);
+    }
+}
 
+function checkpasswords(){
+    let passwordInput = document.getElementById('password').value;
+    let confirmPassword = document.getElementById('confirmPassword').value;
+
+    if(passwordInput!==''||confirmPassword!==''){
+        if(passwordInput!==confirmPassword){
+            Notification.displayNotification('error','Passwords do not match');
+            return false;
+        }else{
+            return true;
+        }
+    }else{
+        return true;
+    }
 }
 
